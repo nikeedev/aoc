4b4ed5bb44a088d5dedffe1e0f9eab0563afc6b5
nikeedev <nikita.gon.dev@gmail.com>
2023-12-25 19:39:25 +0100

    up

--- aoc2015/target/debug/deps/day1-0dcb68a85831e0f6.d
+++
@@ -1,5 +0,0 @@
-/home/nikee/dev/aoc/aoc2015/target/debug/deps/day1-0dcb68a85831e0f6: day1.rs
-
-/home/nikee/dev/aoc/aoc2015/target/debug/deps/day1-0dcb68a85831e0f6.d: day1.rs
-
-day1.rs:
--- aoc2015/target/debug/deps/day1-6df688fa136cfc92.d
+++
@@ -1,9 +0,0 @@
-/home/nikee/dev/aoc/aoc2015/target/debug/deps/libday1-6df688fa136cfc92.rmeta: day1/day1.rs Cargo.toml
-
-/home/nikee/dev/aoc/aoc2015/target/debug/deps/day1-6df688fa136cfc92.d: day1/day1.rs Cargo.toml
-
-day1/day1.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
